55a62e973accaf2dd4a1d1e4aec7d53de140dcee
Peter C Huber <peter@hubersitech.com>
2022-12-19 18:02:27 -0500

    Working on creating the demo model from the C57.91 standard

--- OverloadTemperatures/AppController.swift
+++ OverloadTemperatures/AppController.swift
@@ -13,7 +13,21 @@ class AppController: NSObject {
 
     @IBAction func handle_C57_91_Demo(_ sender: Any) {
         
+        let loss = Losses(conductorType: CU, referenceTemperature: 75.0, coreLoss: 36986.0, coreLossWithOverexcitation: 36986.0, windingResistiveLoss: 51690.0, windingEddyLoss: 0.0, windingHotspotEddyLossPU: 0.0, strayLoss: 21078.0)
         
+        let temperature = Temperatures(ambientTemperature: 20.0, averageWdgTempRise: 63.0, hotspotWdgTempRise: 80.0, topOilRise: 55.0, bottomOilRise: 25.0)
+        
+        // The example in the standard only has the c/c weight, which we need to split into core and winding masses. We use the strange method of calculating MwCpw, then dividing by Cpw to come up with Mw. Of course, we will never need to do this...
+        let massCoreAndCoil = 75600.0
+        let windingTimeConstant = 5.0 // minutes
+        let MwCpw = MCp_W(51690.0, 0.0, windingTimeConstant, temperature.averageFluidTemperatureInCoolingDucts, temperature.averageWindingTemperature)
+        let StdCondChars = StandardConductors
+        // well this is ugly...
+        let Cpw = StdCondChars.0.Cp
+        let massWdg = MwCpw / Cpw
+        let massCore = massCoreAndCoil - massWdg
+        
+        let model = OverloadModel(kvaBaseForTemperatures: 52267.0, kvaBaseForLoss: 28000.0, coolingMode: ONAF, testedTemperatures: temperature, testedLosses: loss, massOfCore: massCore, massOfFluid: 4910.0, massOfTank: 31400, massOfWinding: massWdg)
     }
     
 }
--- OverloadTemperatures/Temperatures.swift
+++ OverloadTemperatures/Temperatures.swift
@@ -9,19 +9,19 @@ import Foundation
 
 struct Temperatures {
     
-    var ambientTemperature:Double = 20.0
+    var ambientTemperature:Double
     
-    var bottomFluidTemperature:Double = 20.0
+    var bottomFluidTemperature:Double
     
-    var averageFluidTemperatureInCoolingDucts:Double = 20.0
-    var averageFluidTemperatureInTankAndRads:Double = 20.0
-    var averageWindingTemperature:Double = 20.0
+    var averageFluidTemperatureInCoolingDucts:Double
+    var averageFluidTemperatureInTankAndRads:Double
+    var averageWindingTemperature:Double
     
-    var topFluidTemperatureInCoolingDucts:Double = 20.0
-    var topFluidTemperatureInTankAndRads:Double = 20.0
-    var hotSpotFluidTemperature:Double = 20.0
+    var topFluidTemperatureInCoolingDucts:Double
+    var topFluidTemperatureInTankAndRads:Double
+    var hotSpotFluidTemperature:Double
     
-    var hotSpotWindingTemperature:Double = 20.0
+    var hotSpotWindingTemperature:Double
     
     // A couple of enums to make it easier to quickly create "standard" temp profiles.
     enum StandardTemp {
@@ -30,9 +30,32 @@ struct Temperatures {
         case std_65_80
     }
     
-    // This is needed to allow defaut initialization
-    init() {
+    // A more useful initializer for our use
+    init(ambientTemperature:Double = 20.0, averageWdgTempRise:Double, hotspotWdgTempRise:Double, topOilRise:Double, bottomOilRise:Double) {
         
+        self.ambientTemperature = ambientTemperature
+        self.bottomFluidTemperature = ambientTemperature + bottomOilRise
+        self.topFluidTemperatureInCoolingDucts = ambientTemperature + topOilRise
+        self.topFluidTemperatureInTankAndRads = self.topFluidTemperatureInCoolingDucts
+        self.hotSpotFluidTemperature = self.topFluidTemperatureInCoolingDucts
+        self.averageWindingTemperature = ambientTemperature + averageWdgTempRise
+        self.hotSpotWindingTemperature = ambientTemperature + hotspotWdgTempRise
+        self.averageFluidTemperatureInCoolingDucts = (topOilRise + bottomOilRise) / 2.0 + ambientTemperature
+        self.averageFluidTemperatureInTankAndRads = self.averageFluidTemperatureInCoolingDucts
+    }
+    
+    // And apparently because we have defined other inits, we have to explicitly create the "default" initializer
+    init(ambientTemperature:Double = 20.0, bottomFluidTemperature:Double = 20.0, averageWindingTemperature:Double = 20.0, topFluidTemperature:Double = 20.0,  hotSpotWindingTemperature:Double = 20.0) {
+        
+        self.ambientTemperature = ambientTemperature
+        self.bottomFluidTemperature = bottomFluidTemperature
+        self.topFluidTemperatureInCoolingDucts = topFluidTemperature
+        self.topFluidTemperatureInTankAndRads = topFluidTemperature
+        self.hotSpotFluidTemperature = topFluidTemperature
+        self.averageFluidTemperatureInCoolingDucts = (topFluidTemperature + bottomFluidTemperature) / 2.0
+        self.averageFluidTemperatureInTankAndRads = (topFluidTemperature + bottomFluidTemperature) / 2.0
+        self.averageWindingTemperature = averageWindingTemperature
+        self.hotSpotWindingTemperature = hotSpotWindingTemperature
     }
     
     // Simple init call to create a dummy temperature profile. The radiator temperature fall (from top to bottom) is arbitrarily set to 20C. The ambient is assumed to be the default (20.0).
@@ -58,12 +81,13 @@ struct Temperatures {
             topFluidRise = 65.0
         }
         
+        self.ambientTemperature = 20.0
         self.hotSpotWindingTemperature = self.ambientTemperature + hotSpotWdgRise
         self.averageWindingTemperature = self.ambientTemperature + averageWdgRise
         self.topFluidTemperatureInTankAndRads = self.ambientTemperature + topFluidRise
         self.topFluidTemperatureInCoolingDucts = self.topFluidTemperatureInTankAndRads
         self.hotSpotFluidTemperature = topFluidTemperatureInCoolingDucts
-        self.bottomFluidTemperature = self.topFluidTemperatureInTankAndRads - 20.0
+        self.bottomFluidTemperature = self.topFluidTemperatureInTankAndRads - topToBottomDiff
         self.averageFluidTemperatureInTankAndRads = (self.topFluidTemperatureInTankAndRads + self.bottomFluidTemperature) / 2.0
         self.averageFluidTemperatureInCoolingDucts = (self.topFluidTemperatureInCoolingDucts + self.bottomFluidTemperature) / 2.0
     }
